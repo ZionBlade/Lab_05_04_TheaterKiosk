48d35f1eef6af8ac3ace672732bdec401d387110
ZionBlade <wernerct@mail.uc.edu>
2022-09-22 13:46:48 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int age = 18;
+
+        System.out.println("Please enter your age");
+
+        if (age >= 21)
+        {
+            System.out.println("Here is your wrist band");
+        }
+    }
+}
